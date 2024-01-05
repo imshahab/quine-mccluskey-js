165b4763f370a97f89dbd1a354fb8266263c9f87
Shahab Hosseini <63445114+theshahabhosseini@users.noreply.github.com>
2024-01-05 22:17:24 +0330

    Code cleanup

--- logic.js
+++ logic.js
@@ -3,34 +3,34 @@
 // The identifyPrimeImplicants function is used to identify the prime implicants in a boolean function.
 // This function takes a list of minterms and iteratively combines them until no more combinations are possible, leaving us with the prime implicants.
 export function identifyPrimeImplicants(inputTerms) {
-  let currentTerms = [...inputTerms];
-  const primeImplicantsList = [];
+  let currentTerms = [...inputTerms]
+  const primeImplicantsList = []
 
   while (currentTerms.length > 1) {
-    const nextRoundTerms = [];
-    const termsMarkedForRemoval = new Array(currentTerms.length).fill(0);
+    const nextRoundTerms = []
+    const termsMarkedForRemoval = new Array(currentTerms.length).fill(0)
 
     for (let i = 0; i < currentTerms.length - 1; i++) {
       for (let j = i + 1; j < currentTerms.length; j++) {
-        const combinedTerm = attemptCombination(currentTerms[i], currentTerms[j]);
+        const combinedTerm = attemptCombination(currentTerms[i], currentTerms[j])
         if (combinedTerm) {
-          nextRoundTerms.push(combinedTerm);
-          termsMarkedForRemoval[i] = termsMarkedForRemoval[j] = 1;
+          nextRoundTerms.push(combinedTerm)
+          termsMarkedForRemoval[i] = termsMarkedForRemoval[j] = 1
         }
       }
     }
 
     for (let i = 0; i < currentTerms.length; i++) {
       if (termsMarkedForRemoval[i] === 0) {
-        primeImplicantsList.push(currentTerms[i]);
+        primeImplicantsList.push(currentTerms[i])
       }
     }
 
-    currentTerms = nextRoundTerms;
+    currentTerms = nextRoundTerms
   }
 
-  primeImplicantsList.push(...currentTerms);
-  return removeDuplicates(primeImplicantsList);
+  primeImplicantsList.push(...currentTerms)
+  return removeDuplicates(primeImplicantsList)
 }
 
 
@@ -61,26 +61,26 @@ export function identifyEssentialPrimeImplicants(inputTerms, primeImplicants) {
 // If they do, it combines them into a new term where the differing bit is replaced with a "-"
 // If they don't, it returns an empty string
 function attemptCombination(term1, term2) {
-  let combinedTerm = "";
-  let differingBitsCount = 0;
+  let combinedTerm = ""
+  let differingBitsCount = 0
 
   for (let i = 0; i < term1.length; i++) {
     if (term1[i] === term2[i]) {
-      combinedTerm += term1[i];
+      combinedTerm += term1[i]
     } else {
-      combinedTerm += "-";
-      differingBitsCount++;
+      combinedTerm += "-"
+      differingBitsCount++
     }
   }
 
-  return differingBitsCount === 1 ? combinedTerm : "";
+  return differingBitsCount === 1 ? combinedTerm : ""
 }
 
 
 // This function removes duplicate items from an array
 // It does this by filtering the array and only keeping items where the first occurrence of the item is at the current index
 function removeDuplicates(arr) { 
-  return arr.filter((item, index) => arr.indexOf(item) === index); 
+  return arr.filter((item, index) => arr.indexOf(item) === index)
 } 
 
 
